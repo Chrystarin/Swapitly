f89d81361bc3107367f8998d25cf4a9e143663cf
Dianne Chrystalin Brandez <chrystarin@gmail.com>
2019-12-22 08:27:11 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,26 +34,28 @@ Route::any('/search',function(){
     $category = Input::get('category');
     $search = Input::get('search');
 
-    if($category === "All"){
-        $user = User::where('first_name','LIKE','%'.$search.'%')->orWhere('last_name','LIKE','%'.$search.'%')->orWhere('username','LIKE','%'.$search.'%')->orWhere('email','LIKE','%'.$search.'%')->get();
-        if(count($user) > 0){
-            return view('pages.search')->withDetails($user)->withQuery ( $search );
-        }
-        else {
-            return view ('pages.search')->withMessage('No '.$search.' found!');
-        }
-    }
-    elseif($category === "Traders"){
-        $user = User::where('first_name','LIKE','%'.$search.'%')->orWhere('last_name','LIKE','%'.$search.'%')->orWhere('username','LIKE','%'.$search.'%')->orWhere('email','LIKE','%'.$search.'%')->get();
-        if(count($user) > 0){
-            return view('pages.search')->withDetails($user)->withQuery ( $search );
-        }
-        else {
-            return view ('pages.search')->withMessage('No '.$search.' found!');
-        }
-    }
-    else{
-        return view ('pages.search')->withMessage('No '.$search.' found!');
+    switch($category){
+        case "All":
+            $user = User::where('first_name','LIKE','%'.$search.'%')->orWhere('last_name','LIKE','%'.$search.'%')->orWhere('username','LIKE','%'.$search.'%')->orWhere('email','LIKE','%'.$search.'%')->get();
+            if(count($user) > 0){
+                return view('pages.search')->withDetails($user)->withQuery($search);
+            }
+            else {
+                return view ('pages.search')->withMessage('There are no results for '.$search.' found');
+            }
+        break;
+        case "Traders":
+            $user = User::where('first_name','LIKE','%'.$search.'%')->orWhere('last_name','LIKE','%'.$search.'%')->orWhere('username','LIKE','%'.$search.'%')->orWhere('email','LIKE','%'.$search.'%')->get();
+            if(count($user) > 0){
+                return view('pages.search')->withDetails($user)->withQuery($search);
+            }
+            else {
+                return view ('pages.search')->withMessage('There are no results for '.$search.' found');
+            }
+        break;
+        default:
+            return view ('pages.search');
+        break;
     }
 
 
