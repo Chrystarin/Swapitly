169ce70152d03c45c27d646502c4c9a259141517
Dianne Chrystalin Brandez <chrystarin@gmail.com>
2020-03-18 15:41:08 +0800

    Fixed Header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,16 +52,22 @@
 						
 					</div>
 					<div id="NavStat_P2">
-                        <div id="NavStat_P2">
-							<div id="NotRegis">
-								<a href="#">Log in</a>
-								<a href="#">Sign up</a>
+                        <!--Code block for changing layout based on user logged in-->
+                        
+                        <!--Guest User-->
+                        @if (Auth::guest())
+                            <div>
+								<a href="{{ route('login') }}">Log in</a>
+								<a href="{{ route('register') }}">Sign up</a>
 							</div>	
-							<div id="Registered">
+                        
+                        <!--Registered User-->
+                        @else
+                            <div id="Registered">
 								<a href="#">Trade now</a>
 								
 								<div id="Img-But">
-									<a href="#" id="Heart"><img src="/storage/images/png/heart.png" alt=""></a>
+									<a href="#" id="Heart"><img src="/storage/images/png/Heart.png" alt=""></a>
 									<ul>
 										<li id="Notification-holder">
 											<img id="Notification" src="/storage/images/png/Notification.png" alt="">
@@ -74,104 +80,6 @@
 
 												</div>
 												<div id="ListOfNotif">
-                                                
-                                                    <a href="#" class="Notif-Lists" data-Type="Request">
-														<li>
-															<img src="/storage/images/sample.jpeg" alt="">
-															<div class="NotifContent">
-																<p><span class="SenderName">Dianne Chrystalin M. Brandez</span c> Sent a trade Request( <span>Moshie's Love</span> - <span>Wife's Love</span> )</p>
-																<p class="Date">Oct 10</p>
-															</div>
-														</li>	
-													</a>
-                                                    <a href="#" class="Notif-Lists" data-Type="Request">
-														<li>
-															<img src="/storage/images/sample.jpeg" alt="">
-															<div class="NotifContent">
-																<p><span class="SenderName">Dianne Chrystalin M. Brandez</span c> Sent a trade Request( <span>Moshie's Love</span> - <span>Wife's Love</span> )</p>
-																<p class="Date">Oct 10</p>
-															</div>
-														</li>	
-													</a>
-                                                    <a href="#" class="Notif-Lists" data-Type="Request">
-														<li>
-															<img src="/storage/images/sample.jpeg" alt="">
-															<div class="NotifContent">
-																<p><span class="SenderName">Dianne Chrystalin M. Brandez</span c> Sent a trade Request( <span>Moshie's Love</span> - <span>Wife's Love</span> )</p>
-																<p class="Date">Oct 10</p>
-															</div>
-														</li>	
-													</a>
-                                                    <a href="#" class="Notif-Lists" data-Type="Request">
-														<li>
-															<img src="/storage/images/sample.jpeg" alt="">
-															<div class="NotifContent">
-																<p><span class="SenderName">Dianne Chrystalin M. Brandez</span c> Sent a trade Request( <span>Moshie's Love</span> - <span>Wife's Love</span> )</p>
-																<p class="Date">Oct 10</p>
-															</div>
-														</li>	
-													</a>
-                                                    
-                                                    
-												</div>
-												<div>
-													<a href="#">Mark as all read</a>
-												</div>
-												
-											</ul>
-										</li>
-									</ul>
-								</div>
-								<ul id="menu">
-									<li>
-										<a href="#">
-											<span id="UserName">Dianne</span>
-											<img src="/storage/images/sample.jpeg" alt="">
-											<img src="image/png/menuMore.png" alt="">
-										</a>
-										<ul id="menuDropDown">
-											<li><a href="#">Profile</a></li>
-											<li><a href="#">My Account</a></li>
-											<li><a href="#">My Trades</a></li>
-											<li><a href="#">Settings</a></li>
-											<li><a href="#">Logout</a></li>
-										</ul>
-									</li>
-								</ul>
-							</div>		
-						</div>
-
-
-
-                        
-                        {{-- <!--Code block for changing layout based on user logged in-->
-                        
-                        <!--Guest User-->
-                        @if (Auth::guest())
-                            <div id="NotRegis">
-								<a href="#">Log in</a>
-								<a href="#">Sign up</a>
-							</div>	
-                        
-                        <!--Registered User-->
-                        @else
-                            <div id="Registered">
-								<a href="#">Trade now</a>
-								
-								<div id="Img-But">
-									<a href="#" id="Heart"><img src="image/png/Heart.png" alt=""></a>
-									<ul>
-										<li id="Notification-holder">
-											<img id="Notification" src="image/png/Notification.png" alt="">
-											<ul id="Notification-DropDown">
-												<div id="Notification-DropDown-header">
-													<p onclick="wew(this)">Notification</p>
-													<span>5</span>
-													<a href="#">All</a>
-													<a href="#" >Request</a>
-
-												</div>
-												<div id="ListOfNotif">
 													<a href="#" class="Notif-Lists" data-Type="Request">
 														<li>
 															<img src="image/received_393147958055492.jpeg" alt="">
@@ -220,21 +128,30 @@
 								<ul id="menu">
 									<li>
 										<a href="#">
-											<span id="UserName">Dianne</span>
-											<img src="image/received_393147958055492.jpeg" alt="">
+											<span id="UserName">{{ Auth::user()->first_name }} </span>
+											<img src="/storage/profile_images/{{Auth::user()->profile_image}}" alt="">
 											<img src="image/png/menuMore.png" alt="">
 										</a>
 										<ul id="menuDropDown">
-											<li><a href="#">Profile</a></li>
+											<li><a href="/profile/{{Auth::user()->id}}">Profile</a></li>
 											<li><a href="#">My Account</a></li>
-											<li><a href="#">My Trades</a></li>
-											<li><a href="#">Settings</a></li>
-											<li><a href="#">Logout</a></li>
+											<li><a href="\products\user">My Trades</a></li>
+											<li><a href="\settings">Settings</a></li>
+											<li>
+												<a href="{{ route('logout') }}"
+													onclick="event.preventDefault();
+															document.getElementById('logout-form').submit();">
+													Logout
+												</a>
+												<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+													{{ csrf_field() }}
+												</form>
+											</li>
 										</ul>
 									</li>
 								</ul>
 							</div>
-                        @endif --}}
+                        @endif
 
 					</div>
 				</div>
