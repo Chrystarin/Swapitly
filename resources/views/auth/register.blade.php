7c1f80c804104ac1e00eaf9701f7ae74568e2485
Dianne Chrystalin Brandez <chrystarin@gmail.com>
2020-04-20 02:01:31 +0800

    Pahabol

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -80,14 +80,11 @@
                 <h6 class="Desc">Confirm Password :</h6>
                 <input id="Confirm-Input" type="password" class="Validation">
             </div>
-            <a id="finish" 
-                    onclick="event.preventDefault();
-                        document.getElementById('SignUp_Form').submit();">>
-                Finish
-            </a>
+
+            {{-- <a id="finish">Finish</a> --}}
 
                 
-            {{-- <button type="submit">Finish</button> --}}
+            <button type="submit">Finish</button>
         </div>
     </form>
     <!-- NOTE * ID for every inputs
